476da70f1e4e91f2d8245d7f7a9b7a105d21548d
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-10-11 14:55:26 +0100

    Add one set of controls with focused proof step

--- src/components/RuleList/index.js
+++ src/components/RuleList/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import cx from 'classnames'
 import Rule from 'components/Rule'
+import RuleControls from 'components/RuleControls'
 import styles from './styles.scss'
 
 export default class RuleList extends Component {
@@ -18,29 +19,22 @@ export default class RuleList extends Component {
 
   moveSelectionUp = (index) => {
     if (index > 0) {
-      this.inputRefs[index - 1].focus()
+      this.onFocus(index - 1)
     }
   }
 
   moveSelectionDown = (index) => {
     if (index < this.inputRefs.length - 1) {
-      this.inputRefs[index + 1].focus()
+      this.onFocus(index + 1)
     }
   }
 
-  createRule = (index) => {
-    this.setState(oldState => {
-      oldState.rules.splice(index + 1, 0, '')
-
-      this.setState({
-        rules: oldState.rules,
-        focusIndex: index + 1,
-      })
-    })
-
-    if (this.inputRefs.length - 1 > index) {
-      this.inputRefs[index + 1].focus()
-    }
+  createRule = () => {
+    this.setState(state => ({
+      rules: [...state.rules, '']
+    }), () =>
+      this.onFocus(this.state.rules.length - 1)
+    )
   }
 
   deleteRule = (index) => {
@@ -59,7 +53,8 @@ export default class RuleList extends Component {
 
     if (index > 0) {
       this.inputRefs[index - 1].focus()
-    } else if (index < this.state.rules.length) {
+    }
+    else if (index < this.state.rules.length) {
       this.inputRefs[index].focus()
     }
   }
@@ -73,25 +68,43 @@ export default class RuleList extends Component {
     })
   }
 
+  onFocus = (index) => {
+    this.setState({ focusIndex: index })
+    this.inputRefs[index].focus()
+  }
+
+  onSelectRule = (symbol) => {
+    this.setState(state => {
+      state.rules[state.focusIndex] += symbol
+      return state
+    })
+    this.inputRefs[this.state.focusIndex].focus()
+  }
+
   render() {
     const { rules } = this.state
 
     return (
-      <ul className={cx(styles.ruleList)}>
+      <React.Fragment>
+        <RuleControls onSelectRule={this.onSelectRule} />
+        <ul className={cx(styles.ruleList)}>
         { rules.map((rule, index) => (
           <Rule
             key={"rule"+index}
             value={rule}
             index={index}
+            isFocus={this.state.focusIndex === index}
             innerRef={(ref) => this.inputRefs[index] = ref}
             onChange={this.onChange}
             createRule={this.createRule}
             deleteRule={this.deleteRule}
             moveSelectionUp={this.moveSelectionUp}
             moveSelectionDown={this.moveSelectionDown}
+            onFocus={this.onFocus}
           />
         )) }
       </ul>
+      </React.Fragment>
     )
   }
 }
