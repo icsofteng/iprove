53a804889f7203a92d40aef7641b47e442c4334e
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-10-15 11:31:46 +0100

    Node.js server talks to z3 binary

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ### Installation
 1. Clone the repository `git clone git@github.com:icsofteng/iprove.git`
 2. Run `npm install` to sync the dependencies.
+3. Run `npm install -g nodemon` to install the node.js watcher
 
 ### Running
-1. In one terminal window, run `npm start`.
+1. In one terminal window, run `nodemon server.js`.
 2. In another terminal, run `npm run build-dev`.
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const http = require('http');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const http = require('http')
+const { exec } = require('child_process')
+const fs = require('fs')
 
 // Configuration
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
+
+app.get('/z3', (req, res) => {
+  const code = req.query.code
+  fs.writeFile("tmp", code, () => {
+    const cmd = './z3 tmp'
+    exec(cmd, (err, stdout) => {
+      fs.unlink('tmp', () =>
+        res.send(stdout)
+      )
+    })
+  })
+})
 
 // Start server
-const port = process.env.PORT || 8080;
-http.createServer(app).listen(port);
-console.log('Listening on port ' + port);
+const port = process.env.PORT || 8080
+http.createServer(app).listen(port)
+console.log('Listening on port ' + port)
