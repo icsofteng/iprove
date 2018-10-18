4b6bf686d2c9858ca5b6d9b7cc187092b6333c5f
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-10-18 15:35:07 +0100

    Fix issues with true/false

---
+++ src/translator/test/True.test.js
@@ -0,0 +1,22 @@
+const {translate} = require('../../translator')
+
+const test_constants = ['p']
+const test_rules = [{
+  type: 'literal',
+  value: 'p'
+}, {
+  type: 'binary',
+  symbol: 'implies',
+  lhs: {
+    type: 'literal',
+    value: 'p'
+  },
+  rhs: {
+    type: 'true'
+  },
+}]
+
+
+test('True test', () => {
+  expect(translate(test_rules, test_constants)).toMatchSnapshot()
+})
