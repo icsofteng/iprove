ca8f66d26f6449775541b1c68b0bc6d787b3d08b
Daniel Yung <danielyung211@gmail.com>
2018-10-22 12:40:57 +0100

    Ruleplaceholder test missing

---
+++ src/components/RulePlaceholder/RulePlaceholder.test.js
@@ -0,0 +1,7 @@
+// import React from 'react'
+// import RulePlaceholder from './'
+
+// test('RulePlaceholder test', () => {
+//   const wrapper = shallow(<RulePlaceholder path={[]} index={0}/>)
+//   expect(wrapper).toMatchSnapshot()
+// })
