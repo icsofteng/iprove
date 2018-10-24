c7acfdb76257765dc533fb1346a058b2a9d079d7
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-10-24 20:08:27 +0100

    Clean up Controls component

--- src/components/Basic/Controls/index.js
+++ src/components/Basic/Controls/index.js
@@ -2,47 +2,23 @@ import React from 'react'
 import cx from 'classnames'
 import styles from './styles.scss'
 
+const ControlBlock = ({ type, label, symbol, exprLeft=false, exprRight }) =>
+  <div data-type={type} className={cx('drag-drop', styles.controlBlock)}>
+    <div className={styles.label}>{label}</div>
+    <div className={styles.template}>
+      { exprLeft && <div className={styles.exprPlaceholder}></div> }
+      <div className={styles.symbolPlaceholder}>{symbol}</div>
+      { exprRight && <div className={styles.exprPlaceholder}></div> }
+    </div>
+  </div>
+
 const Controls = () =>
   <div className={styles.controlList}>
-    
-    <div data-type="binary" className={cx('drag-drop', styles.controlBlock)}>
-      <div className={styles.label}>Binary Rule</div>
-      <div className={styles.template}>
-        <div className={styles.exprPlaceholder}></div>
-        <div className={styles.symbolPlaceholder}>?</div>
-        <div className={styles.exprPlaceholder}></div>
-      </div>
-    </div>
-
-    <div data-type="unary" className={cx('drag-drop', styles.controlBlock)}>
-      <div className={styles.label}>Unary Rule</div>
-      <div className={styles.template}>
-        <div className={styles.symbolPlaceholder}>?</div>
-        <div className={styles.exprPlaceholder}></div>
-      </div>
-    </div>
-
-    <div data-type="literal" className={cx('drag-drop', styles.controlBlock)}>
-      <div className={styles.label}>Literal</div>
-      <div className={styles.template}>
-        <div className={styles.symbolPlaceholder}>x</div>
-      </div>
-    </div>
-
-    <div data-type="true" className={cx('drag-drop', styles.controlBlock)}>
-      <div className={styles.label}>True</div>
-      <div className={styles.template}>
-        <div className={styles.symbolPlaceholder}>⊤</div>
-      </div>
-    </div>
-
-    <div data-type="false" className={cx('drag-drop', styles.controlBlock)}>
-      <div className={styles.label}>False</div>
-      <div className={styles.template}>
-        <div className={styles.symbolPlaceholder}>⊥</div>
-      </div>
-    </div>
-
+    <ControlBlock type="binary" label="Binary Rule" symbol="?" exprLeft exprRight />
+    <ControlBlock type="unary" label="Unary Rule" symbol="?" exprRight />
+    <ControlBlock type="literal" label="Literal" symbol="x" />
+    <ControlBlock type="true" label="True" symbol="⊤" />
+    <ControlBlock type="false" label="False" symbol="⊥" />
   </div>
 
 export default Controls
