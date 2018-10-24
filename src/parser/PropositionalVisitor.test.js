38472d286c3ec690a4915f78bda145a0eab43767
Daniel Yung <danielyung211@gmail.com>
2018-10-24 11:38:46 +0100

    Fixed broken test cases

--- src/parser/PropositionalVisitor.test.js
+++ src/parser/PropositionalVisitor.test.js
@@ -1,43 +1,45 @@
 import {parse} from './'
-import { notDeepEqual } from 'assert';
 
-const p = {type: 'literal', value: "p" }
-const q = {type: 'literal', value: "q" }
+const p = {type:'literal', value:"P" }
+const q = {type:'literal', value:"Q" }
 
 let value
 test("Visitor Test implies", ()=> {
-  expect(parse("p implies q")).toEqual([{type: 'binary', symbol: 'implies', lhs: p, rhs:q}])
+  expect(parse("P implies Q")).toEqual({ast:[{type:'binary', symbol:'implies', lhs: p, rhs:q}], constants:["P", "Q"]})
 })
 
 test("Visitor Test and", ()=> {
-  expect(parse("p and q")).toEqual([{type: 'binary', symbol: 'and', lhs: p, rhs:q}])
+  expect(parse("P and Q")).toEqual({ast:[{type:'binary', symbol:'and', lhs:p, rhs:q}], constants:["P", "Q"]})
 })
 
 test("Visitor Test iff", ()=> {
-  expect(parse("p iff q")).toEqual([{type: 'binary', symbol: 'iff', lhs: p, rhs:q}])
+  expect(parse("P iff Q")).toEqual({ast:[{type:'binary', symbol:'iff', lhs:p, rhs:q}], constants:["P", "Q"]})
 })
 
 test("Visitor Test or", ()=> {
-  expect(parse("p or q")).toEqual([{type: 'binary', symbol: 'or', lhs: p, rhs:q}])
+  expect(parse("P or Q")).toEqual({ast:[{type:'binary', symbol:'or', lhs:p, rhs:q}], constants:["P", "Q"]})
 })
 
 test("Visitor Test not", ()=> {
-  expect(parse("not p")).toEqual([{type: 'unary', symbol: 'not', value: p}])
+  expect(parse("not P")).toEqual({ast:[{type:'unary', symbol:'not', value:p}], constants:["P"]})
 })
 
 test("Visitor Test literals", ()=> {
-  expect(parse("p")).toEqual([p])
-  expect(parse("true")).toEqual([{type: 'true'}])
-  expect(parse("false")).toEqual([{type: 'false'}])
+  expect(parse("P")).toEqual({ast:[p], constants:["P"]})
+  expect(parse("true")).toEqual( {ast:[{type:'true'}], constants:[]})
+  expect(parse("false")).toEqual( {ast:[{type:'false'}], constants:[]})
 })
 
 test("Visitor Integrated Test", () => {
-  let x = {type: 'literal', value: "x" }
-  let y = {type: 'literal', value: "y" }
+  const x = {type:'literal', value:"X" }
+  const y = {type:'literal', value:"Y" }
   value = x
-  let lhs2 = {type: 'binary', symbol: 'and', lhs:p, rhs:q}
-  let rhs2 = {type: 'binary', symbol: 'or', lhs:{type: "unary", symbol: "not", value: x },rhs: y}
-  expect(parse("(p and q) implies (not x or y)")).toEqual(
-    [{type: 'binary', symbol: 'implies', lhs:lhs2 , rhs:rhs2}]
-    )
+  const lhs2 = {type:'binary', symbol:'and', lhs:p, rhs:q}
+  const rhs2 = {type:'binary', symbol:'or', lhs:{type:"unary", symbol:"not", value:x }, rhs:y}
+  const bracketL = {type:"paren", value:lhs2}
+  const bracketR = {type:"paren", value:rhs2}
+
+  expect(parse("(P and Q) implies (not X or Y)")).toEqual(
+    {ast:[{type:'binary', symbol:'implies', lhs:bracketL , rhs:bracketR}], constants:["P", "Q", "X", "Y"]}
+  )
 })
