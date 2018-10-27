9a9a193be6863eadb6474cb0fce3709ee4769401
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-10-27 22:25:01 +0100

    Fix tests

--- src/translator/test/And.test.js
+++ src/translator/test/And.test.js
@@ -3,26 +3,20 @@ const {translate} = require('../../translator/z3')
 const test_constants = ['p', 'q']
 const test_rules = [
   {
-    dependencies: [],
-    ast: {
-      type: 'binary',
-      symbol: 'and',
-      lhs: {
-        type: 'literal',
-        value: 'p'
-      },
-      rhs: {
-        type: 'literal',
-        value: 'q'
-      },
-    }
-  },
-  {
-    dependencies: [], 
-    ast: { 
+    type: 'binary',
+    symbol: 'and',
+    lhs: {
       type: 'literal',
       value: 'p'
-    }
+    },
+    rhs: {
+      type: 'literal',
+      value: 'q'
+    },
+  },
+  { 
+    type: 'literal',
+    value: 'p'
   }
 ]
 
--- src/translator/test/QuantifierExist.test.js
+++ src/translator/test/QuantifierExist.test.js
@@ -1,33 +1,26 @@
 const {translate} = require('../z3')
 const test_constants = ['p']
-const relations = []
 const test_rules = [
   {
-    dependencies: [],
-    ast: {
-      type: 'quantifier',
-      symbol: 'exist',
-      variable: "x", 
-      value: { 
-        type: 'binary',
-        symbol: 'implies',
-        lhs: {
-          type: 'literal',
-          value: 'x'
-        },
-        rhs: {
-          type: 'literal',
-          value: 'p'
-        }
+    type: 'quantifier',
+    symbol: 'exist',
+    variable: "x", 
+    value: { 
+      type: 'binary',
+      symbol: 'implies',
+      lhs: {
+        type: 'literal',
+        value: 'x'
+      },
+      rhs: {
+        type: 'literal',
+        value: 'p'
       }
     }
   },
   {
-    dependencies: [],
-    ast: {
-      type: 'literal',
-      value: 'p'
-    }
+    type: 'literal',
+    value: 'p'
   }  
 ]
 
