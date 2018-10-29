dc7f5d5677f116f74342d4a4d678492494bc385c
Daniel Yung <danielyung211@gmail.com>
2018-10-29 14:35:59 +0000

    fixed test cases

--- src/parser/FirstOrderVisitor.test.js
+++ src/parser/FirstOrderVisitor.test.js
@@ -2,25 +2,25 @@ import {parse} from './'
 
 const dragonX = { type: 'relation', name:"dragon", params:[{type:"variable", value:"x"}] }
 const humanXYZ = { type: 'relation', name:"human", params:[{type:"variable", value:"x"}, {type:"variable", value:"y"}, {type:"variable", value:"z"}] }
-const Frank = { type: 'relation', name:"human", params:[{type:"constant", value:"Frank"}]}
+const Frank = { type: 'relation', name:"person", params:[{type:"constant", value:"Frank"}]}
 
 
 test("Visitor Test relations", ()=> {
-  expect(parse("dragon(x)")).toEqual({ast:[dragonX], constants:[]})
+  expect(parse("dragon(x)")).toEqual({ast:[dragonX], constants:[],relations: [{name: "dragon", numParam: 1}]})
 })
 
 test("Visitor Test forall", ()=> {
-  expect(parse("forall x dragon(x)")).toEqual({ast:[{symbol:"forall", type: "quantifier", value: dragonX, variable: "x"}], constants:[]})
+  expect(parse("forall x dragon(x)")).toEqual({ast:[{symbol:"forall", type: "quantifier", value: dragonX, variable: "x"}], constants:[], relations: [{name: "dragon", numParam: 1}]})
 })
 
 test("Visitor Test exists", ()=> {
-  expect(parse("exists x dragon(x)")).toEqual({ast:[{symbol:"exists", type: "quantifier", value: dragonX, variable: "x"}], constants:[]})
+  expect(parse("exists x dragon(x)")).toEqual({ast:[{symbol:"exists", type: "quantifier", value: dragonX, variable: "x"}], constants:[], relations: [{name: "dragon", numParam: 1}]})
 })
 
 test("Visitor Test params", ()=> {
-  expect(parse("human(x, y, z)")).toEqual({ast:[humanXYZ], constants:[]})
+  expect(parse("human(x, y, z)")).toEqual({ast:[humanXYZ], constants:[], relations: [{name: "human", numParam: 3}]})
 })
 
-test("Visitor Test params", ()=> {
-  expect(parse("human(Frank)")).toEqual({ast:[Frank], constants:[]})
+test("Visitor Test ", ()=> {
+  expect(parse("person(Frank)")).toEqual({ast:[Frank], constants:[], relations: [{name: "person", numParam: 1}]})
 })
