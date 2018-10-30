61013bd2afb697304a72d4436a590282dc1a0d2d
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-10-30 13:54:22 +0000

    Cleanup

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + '/public'))
 app.post('/z3', (req, res) => {
   const { steps, constants, relations } = req.body
   const file = translate_and_save(steps, constants, relations)
-  const cmd = 'z3 ' + file
+  const cmd = './z3 ' + file
   exec(cmd, (err, stdout) => {
     fs.unlink(file, () =>
       res.send(stdout)
