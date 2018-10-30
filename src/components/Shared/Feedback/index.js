762c76c59baf83638570662023a96596784095d1
Tim Green <tiimgreen@gmail.com>
2018-10-30 13:14:31 +0000

    clean up Feedback component

--- src/components/Shared/Feedback/index.js
+++ src/components/Shared/Feedback/index.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import cx from 'classnames'
+
 import styles from './styles.scss'
 
-const Feedback = ({ z3 }) =>
-  z3 &&
-  <div className={cx(styles.feedback, {
-    [styles.error]: z3 !== 'unsat'
-  })}>
-  {
-    (z3 === 'sat' ? "Your proof is incorrect" :
-      (
-        z3 === 'unsat' ? "Your proof is correct, well done!" :
-        "There are errors in your proof"
-      )
+class Feedback extends Component {
+  getFeedbackText() {
+    const { z3, steps } = this.props
+
+    if (steps.length <= 1) {
+      return "Please enter more than one step to validate your proof"
+    }
+
+    if (z3 === 'sat') {
+      return "Your proof is incorrect"
+    }
+
+    if (z3 === 'unsat') {
+      return "Your proof is correct, well done!"
+    }
+
+    return "There are errors in your proof"
+  }
+
+  render() {
+    const { z3 } = this.props
+
+    return (
+      <div className={cx(styles.feedback, { [styles.error]: z3 !== 'unsat' })}>
+        { this.getFeedbackText() }
+      </div>
     )
   }
-  </div>
+}
 
 export default Feedback
