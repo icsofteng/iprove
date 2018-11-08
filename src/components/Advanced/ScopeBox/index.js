c16105802df082ff61447a42e8edba25a11ba2c3
Alexander Zakon <azakon1997@gmail.com>
2018-11-08 16:22:51 +0000

    Generate recursively text box list and scope box

--- src/components/Advanced/ScopeBox/index.js
+++ src/components/Advanced/ScopeBox/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import TextBoxList from '../TextBoxList'
 import styles from './styles.scss'
 
 class ScopeBox extends Component {
-
   render() {
     return (
-      <TextBoxList {...this.props}/>
+      <div className={styles.scopeBox}>
+        {this.props.children}
+      </div>
     )
   }
 }
