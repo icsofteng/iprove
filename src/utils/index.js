131ee2690b3abe4d3ceedd81b6ec98950c910022
Jason Lipowicz <jason.lipowicz16@imperial.ac.uk>
2018-11-16 11:28:01 +0000

    Login reasoning from case analysis

--- src/utils/index.js
+++ src/utils/index.js
@@ -30,7 +30,7 @@ const equal_ast = (first, second) => {
   return _.isEqual(modifiedFirst, modifiedSecond)
 }
 
-const is_valid_dependency = (step, dependencyStep) => {
+const is_valid_dependency = (step, dependencyStep, allSteps, givens) => {
   // Make sure no dependencies have a scope that the step's scope contains
   if (dependencyStep.scope.filter(s => step.scope.indexOf(s) === -1).length === 0) {
     return true
@@ -43,17 +43,53 @@ const is_valid_dependency = (step, dependencyStep) => {
      (_.isEqual(dependencyStep.scope.slice(0, -1), step.scope))) {
     return true
   }
+  // Allow case analysis
+  if ((dependencyStep.ast.type !== 'case' && step.dependencies.filter(d => calculate_dependency_step(allSteps, d, givens).ast.type === 'case').length > 0) ||
+      (dependencyStep.ast.type === 'case' && _.isEqual(dependencyStep.scope, step.scope) &&
+      step.dependencies
+        .filter(d => calculate_dependency_step(allSteps, d, givens).ast.type === 'assume')
+        .filter(d => {
+          let dStep = calculate_dependency_step(allSteps, d, givens)
+          let scopeEqual = _.isEqual(dStep.scope.slice(0, -1), step.scope)
+          let matchesLhs = _.isEqual(dStep.ast.value, dependencyStep.ast.lhs)
+          let matchesRhs = _.isEqual(dStep.ast.value, dependencyStep.ast.rhs)
+          return scopeEqual && (matchesLhs || matchesRhs)
+        }).length === 2 &&
+      step.dependencies
+        .filter((d, i) => {
+          let dStep = calculate_dependency_step(allSteps, d, givens)
+          if (_.isEqual(dStep.scope.slice(0, -1), step.scope)) {
+            for (let j in step.dependencies) {
+              if (j !== i && _.isEqual(calculate_dependency_step(allSteps, step.dependencies[j], givens).ast, dStep.ast)) {
+                return _.isEqual(calculate_dependency_step(allSteps, step.dependencies[j], givens).scope.slice(0, -1), dStep.scope.slice(0, -1))
+              }
+            }
+          }
+          return false
+        }).length >= 1
+      )) {
+    return true
+  }
   return false
 }
 
-const validate_dependencies = (step, dependency, givens, allSteps) => {
+const calculate_dependency_step = (steps, dependency, givens) => {
   if (dependency <= givens.length) {
-    return (givens[dependency-1] && givens[dependency-1].ast) || null
+    return givens[dependency-1]
+  }
+  else {
+    return steps[dependency-givens.length-1]
+  }
+}
+
+const validate_dependencies = (step, dependency, givens, allSteps) => {
+  const dependencyStep = calculate_dependency_step(allSteps, dependency, givens)
+  if (dependency <= givens.length) {
+    return (dependencyStep && dependencyStep.ast) || null
   }
   else {
     // Using a step dependency, check scope is valid
-    const dependencyStep = allSteps[dependency-givens.length-1]
-    if (is_valid_dependency(step, dependencyStep)) {
+    if (is_valid_dependency(step, dependencyStep, allSteps, givens)) {
       return (dependencyStep && dependencyStep.ast) || null
     }
     return null
