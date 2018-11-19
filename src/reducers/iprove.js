df28f7182d1b19d64462e1ae79daa858004017da
Ross Hunter <rossxhunter@gmail.com>
2018-11-19 11:46:35 +0000

    Fixed deletion bug

--- src/reducers/iprove.js
+++ src/reducers/iprove.js
@@ -86,9 +86,28 @@ const reducer = (state = initialState, action) => {
         return newState
 
       case INSERT_STEP:
-          scope = (key === 'steps') ? newState.currentScope : []
-          depth.splice(index, 0, { scope, dependencies: [], ast: { type: action.payload, ...action.otherArgs } })
-          return newState
+        scope = (key === 'steps') ? newState.currentScope : []
+        depth.splice(index, 0, {
+          scope,
+          dependencies: [],
+          ast: {
+            type: action.payload,
+            ...action.otherArgs
+          }
+        })
+        return { ...newState,
+          steps: newState.steps.filter(Boolean)
+        }
+
+      case REMOVE_STEP:
+        if (Array.isArray(depth)) {
+          depth.splice(index + 1, 1)
+        } else {
+          delete depth[index]
+        }
+        return { ...newState,
+          steps: newState.steps.filter(Boolean)
+        }
 
       case NEW_RULE:
         depth[index] = { type: action.payload,...action.otherArgs }
