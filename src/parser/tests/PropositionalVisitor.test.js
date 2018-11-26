5ef875b3d07392f4f88bd57c98f73c5d7e69f699
Daniel <danielyung211@gmail.com>
2018-11-26 17:52:00 +0000

    made test cases into snapshots testing

--- src/parser/tests/PropositionalVisitor.test.js
+++ src/parser/tests/PropositionalVisitor.test.js
@@ -1,43 +1,31 @@
 import {parse} from '../'
 
-const p = {type:'identifier', value:"P", varType: "Bool" }
-const q = {type:'identifier', value:"Q", varType: "Bool" }
-
 test("Visitor Test implies", ()=> {
-  expect(parse("P implies Q")).toEqual({ast:[{type:'binary', symbol:'implies', lhs: p, rhs:q}], identifiers: [{ value: "P", varType: "Bool"}, {value: "Q", varType: "Bool"}], "relations":[], functions: [], types:[]})
+  expect(parse("P implies Q")).toMatchSnapshot()
 })
 
 test("Visitor Test and", ()=> {
-  expect(parse("P and Q")).toEqual({ast:[{type:'binary', symbol:'and', lhs:p, rhs:q}], identifiers: [{ value: "P", varType: "Bool"}, {value: "Q", varType: "Bool"}], "relations":[], functions: [], types:[]})
+  expect(parse("P and Q")).toMatchSnapshot()
 })
 
 test("Visitor Test iff", ()=> {
-  expect(parse("P iff Q")).toEqual({ast:[{type:'binary', symbol:'iff', lhs:p, rhs:q}], identifiers: [{ value: "P", varType: "Bool"}, {value: "Q", varType: "Bool"}], "relations":[], functions: [], types:[]})
+  expect(parse("P iff Q")).toMatchSnapshot()
 })
 
 test("Visitor Test or", ()=> {
-  expect(parse("P or Q")).toEqual({ast:[{type:'binary', symbol:'or', lhs:p, rhs:q}], identifiers: [{ value: "P", varType: "Bool"}, {value: "Q", varType: "Bool"}], "relations":[], functions: [], types:[]})
+  expect(parse("P or Q")).toMatchSnapshot()
 })
 
 test("Visitor Test not", ()=> {
-  expect(parse("not P")).toEqual({ast:[{type:'unary', symbol:'not', value:p}], identifiers: [{ value: "P", varType: "Bool"}], "relations":[], functions: [], types:[]})
+  expect(parse("not P")).toMatchSnapshot()
 })
 
 test("Visitor Test literals", ()=> {
-  expect(parse("P")).toEqual({ast:[p], identifiers: [{ value: "P", varType: "Bool"}], "relations":[], functions: [], types:[]})
-  expect(parse("true")).toEqual( {ast:[{type:'true'}], identifiers: [], "relations":[], functions: [], types:[]})
-  expect(parse("false")).toEqual( {ast:[{type:'false'}], identifiers: [], "relations":[], functions: [], types:[]})
+  expect(parse("P")).toMatchSnapshot()
+  expect(parse("true")).toMatchSnapshot()
+  expect(parse("false")).toMatchSnapshot()
 })
 
 test("Visitor Integrated Test", () => {
-  const x = {type:'identifier', value:"X", varType: "Bool" }
-  const y = {type:'identifier', value:"Y", varType: "Bool" }
-  const lhs2 = {type:'binary', symbol:'and', lhs:p, rhs:q}
-  const rhs2 = {type:'binary', symbol:'or', lhs:{type:"unary", symbol:"not", value:x }, rhs:y}
-  const bracketL = {type:"paren", value:lhs2}
-  const bracketR = {type:"paren", value:rhs2}
-
-  expect(parse("(P and Q) implies (not X or Y)")).toEqual(
-    {ast:[{type:'binary', symbol:'implies', lhs:bracketL , rhs:bracketR}], identifiers: [{ value: "P", varType: "Bool"}, {value: "Q", varType: "Bool"}, {value: "X", varType: "Bool"}, {value: "Y", varType: "Bool"}], "relations":[], functions: [], types:[]}
-  )
+  expect(parse("(P and Q) implies (not X or Y)")).toMatchSnapshot()
 })
