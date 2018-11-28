9030b16d7966b7ec895c9944261c2b1843452ea3
Alexander Zakon <azakon1997@gmail.com>
2018-11-28 16:58:32 +0000

    Save button closes the ModalAddLemma

---
+++ src/components/Shared/ModalAddLemma/index.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import styles from './styles.scss'
+import TextBox from '../../Advanced/TextBox'
+
+
+export default class ModalAddLemma extends Component {
+  render() {
+    return <React.Fragment>
+      <div className={styles.overlay} onClick={()=>this.props.onCancel()}></div>
+      <div className={styles.panelBox}>
+        <div className={styles.panelTitle}>Add a lemma</div>
+        <div className={styles.panelContent}>
+          {
+            <TextBox
+            key={"lemma"}
+            ast= {{}}
+            focus={true}
+            z3={this.props.z3}
+          />  
+          }
+        </div>
+        <div>
+          <button
+            onClick={() => this.props.onCancel() }>
+            Save
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
+  }
+
+}
