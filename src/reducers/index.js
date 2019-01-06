89d535a4c2590d975a50cee813cd12bebe6d3774
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2019-01-06 23:50:02 +0000

    Remove debug from reducer undo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,6 @@ import iprove from './iprove'
 import { ADD_IDENTIFIERS, ADD_ATOMS, ADD_RELATIONS, ADD_TYPES, SET_SELECTED, SET_GOAL_ACHIEVED, ADD_FUNCTIONS, TOGGLE_SECTION, UPDATE_RULE } from '../constants';
 
 export default undoable(iprove, {
-  debug: true,
   filter: combineFilters(
     excludeAction([ADD_IDENTIFIERS, ADD_ATOMS, ADD_RELATIONS, ADD_FUNCTIONS, ADD_TYPES, SET_SELECTED, SET_GOAL_ACHIEVED, TOGGLE_SECTION]),
     (action) => action.type !== UPDATE_RULE || (action.type === UPDATE_RULE && action.path.slice(-1)[0] === "raw")
